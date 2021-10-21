b2f429f1beec16be9870d452bd14783f61a1f792
ChanYongKeong <92508456+ChanYongKeong@users.noreply.github.com>
2021-10-21 13:15:57 +0800

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,32 @@
+<img src="banner.jpg" alt="banner"/>
+<style>
+nav {
+	width: 100%;
+	background-color: #000000;
+	overflow: auto;
+}
+
+nav a {
+	float: left;
+	padding: 10px;
+	color: #0000ff;
+	text-decoration: none;
+	font-size: 15px;
+	width: 20%;
+	text-align: center;
+}
+
+nav a:hover {
+	color: #ff0000;
+}
+
+img {
+  width: 100%;
+}
+</style>
+<nav>
+<a href="logcall.php">Log Call</a>
+<a href="Update.php">Update</a>
+<a href="#">Report</a>
+<a href="#">History</a>
+</nav>
